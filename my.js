27e4a0edb34f9f987cf1c0459dbc8e179627f259
ACDos2021 <91865032+ACDos2021@users.noreply.github.com>
2021-10-04 13:37:02 +0800

    Update my.js

--- my.js
+++ my.js
@@ -11,3 +11,24 @@ function textGS(){
 }
 
 document.getElementById("btn").addEventListener("click",textGS);
+
+function addGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbxUMfidmDq50zRYHweiEHNnbEzPg_goPDv-yyO3Hh4gckA2WLbR/exec"
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer',
+        body: JSON.stringify({name:"Joe"})
+    });
+
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
